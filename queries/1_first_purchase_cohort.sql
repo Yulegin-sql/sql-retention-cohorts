10e16a3c970709153cca2eddd6037cc559dac64e
Gleb <gleb.yulegin@mail.ru>
2026-09-26 02:11:26 +0300

    Обновил запрос: немного подправил
    Немного подправил запрос

--- queries/1_first_purchase_cohort.sql
+++ queries/1_first_purchase_cohort.sql
@@ -1,8 +1,15 @@
 -- Запрос №1: Месяц первой покупки каждого клиента
 -- Автор: Глеб Юлегин
 -- Дата: 2026-09-25
-with first_purchase as (select customer_id, min(date_trunc('month', invoice_date)) as first_purchase_month from retail
-where quantity > 0 and price > 0 and customer_id is not null group by customer_id)
+with first_purchase as (
+select 
+customer_id, 
+min(date_trunc('month', invoice_date)) as first_purchase_month 
+from retail
+where quantity > 0
+and price > 0
+and customer_id is not null 
+group by customer_id)
 select first_purchase_month, COUNT(*) as cohort_size
 from first_purchase
 group by first_purchase_month
